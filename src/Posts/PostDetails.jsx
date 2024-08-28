760b5def36206d219e3606080774aa672d24290c
Cody Clark <codyjamesclark@gmail.com>
2024-08-28 13:33:31 -0500

    added view for post details

--- src/Posts/PostDetails.jsx
+++ src/Posts/PostDetails.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react"
 import { getPostById } from "../managers/PostManager"
 import { useParams } from "react-router-dom"
+import "./Posts.css"
 
 export const PostDetails = () => {
 
@@ -12,4 +13,14 @@ export const PostDetails = () => {
             setPost(data)
         })
     }, [postId])
+
+    return (
+        <div className="details-container">
+            <div className="details-title">{post.title}</div>
+            <img src={post.image_url} alt={post.title} className="details-img"></img>
+            <div className="details-content">{post.content}</div>
+            <div className="details-date">Published on: {post.publication_date}</div>
+            <div className="details-author">Written by: {post.first_name} {post.last_name}</div>
+        </div>
+    )
 }
